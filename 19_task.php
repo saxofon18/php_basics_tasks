24c6744d27eb6d5d05ac6159f8f20c29d78f77b5
Andrey Seriy <saxofon18@ukr.net>
2017-07-04 16:24:38 +0300

    homework php 1

---
+++ 19_task.php
@@ -0,0 +1,34 @@
+
+  <?php
+ /*
+   19. Выведите результат сравнения $a и $b из п.17 с помощью var_dump.
+   
+   */
+  
+ $a = '68';
+ $b =78;
+ 
+ if($a==$b){
+     echo "Числа равны";
+ } else {
+     echo "Не равны";
+ }
+
+ echo "<br>";
+ 
+ var_dump ($a);
+ 
+ echo "<br>";
+ 
+ var_dump ($b);
+ 
+ echo "<br>";
+ 
+ var_dump ($a);
+ 
+ echo "<br>";
+ 
+ var_dump ($b);
+  
+  
+  
---
+++ 26_task.php
@@ -0,0 +1,12 @@
+
+  <?php
+ /*
+   26. Объявите константу DAYS_COUNT равную 7 и константу MONTH_COUNT 
+   равную 12 двумя разными способами объявления констант
+   */
+  
+  define('DAYS_COUNT',7);
+  echo DAYS_COUNT ."<br>" ;
+
+  const MONTH_COUNT=12;
+  echo MONTH_COUNT;
